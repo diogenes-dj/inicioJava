9b986bfddc2e2e9162cef99294feee0e1a8bd215
Diogenes <zyguifried@hotmail.com>
2022-10-19 16:13:26 -0300

    Adicionando dados dos primeiros passos!

---
+++ TiposDeDadosCasting/src/Inicio/Main.java
@@ -0,0 +1,35 @@
+package Inicio;
+
+/**
+ *  Classe de exemplo de variáveis, tipos de dados e operadores aritméticos
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        byte b1;
+        short s1 = 1000;
+        b1 = (byte) s1;
+
+        long l1;
+        int i1 = 10;
+        l1 = i1;
+
+        int i2;
+        long l2 = 1000000000000000000L;
+        i2 = (int) l2;
+
+        int i3;
+        long l3 = 10000L;
+        i3 = (int) l3;
+
+        double d1;
+        float f1 = 10.5f;
+        d1 = f1;
+
+        float f2;
+        float f3;
+        double d2 = 10000.58d;
+        f2 = (float) d2;
+    }
+}
